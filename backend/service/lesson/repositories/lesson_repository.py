2c1e23a89d88f5482a370bebcbd735e522522932
sergeMMikh <s.m.mih@mail.ru>
2024-10-29 22:14:26 +0000

    added some coments for lessons services

--- backend/service/lesson/repositories/lesson_repository.py
+++ backend/service/lesson/repositories/lesson_repository.py
@@ -50,17 +50,28 @@ class LessonRepository(BaseRepository):
         return result
 
     async def get_all_lesson_by_filter(self, filters: LessonFilterSchema, student_id: Optional[int] = None):
-        stmt = (
-            select(self.model)
+        """
+         асинхронный метод для получения списка уроков с использованием фильтров. 
+         Она формирует SQL-запрос, учитывая заданные параметры фильтрации, и возвращает 
+         отфильтрованный список записей с дополнительной информацией. 
+        """
+
+        # Базовый запрос
+        stmt = ( 
+            select(self.model) # создает базовый запрос, выбирая все записи из модели self.model (предположительно, модели уроков).
             .options(
+                # options(joinedload(...)) — используется для загрузки связанных данных с joinedload, 
+                # чтобы предотвратить "ленивую загрузку" и получить сразу всю необходимую информацию
+                #  (например, данные тренера, пространства, проверки).
                 joinedload(self.model.trainer),
                 joinedload(self.model.space),
                 joinedload(self.model.check).joinedload(Check.training_data),
                 joinedload(self.model.check).joinedload(Check.student),
             )
-            .filter(self.model.deleted.__eq__(False))
+            .filter(self.model.deleted.__eq__(False)) # исключает удаленные записи 
+            # (предполагается, что deleted — это флаг, который отмечает, была ли запись удалена).
         )
-        if filters.search_string:
+        if filters.search_string: # Если filters.search_string задан, добавляются условия поиска по подстроке, игнорируя регистр:
             stmt = stmt.filter(or_(
                 self.model.space.has(Space.name.ilike(f"%{filters.search_string}%")),
                 self.model.trainer.has(User.firstname.ilike(f"%{filters.search_string}%")),
@@ -68,15 +79,16 @@ class LessonRepository(BaseRepository):
                 self.model.trainer.has(User.surname.ilike(f"%{filters.search_string}%"))
                 )
             )
-        if filters.start_date:
+        if filters.start_date: # проверяет, что урок начался не раньше этой даты.
             stmt = stmt.filter(self.model.start >= filters.start_date.replace(tzinfo=None))
-        if filters.end_date:
+        if filters.end_date: # ограничивает дату начала урока, чтобы она была меньше заданной конечной даты.
             stmt = stmt.filter(self.model.start < filters.end_date.replace(tzinfo=None))
-        if filters.trainer:
+        if filters.trainer: # фильтрация по ID тренера
             stmt = await self._add_filters(stmt, trainer_id=filters.trainer)
-        if student_id:
+        if student_id: # Фильтрация по ID студента
             stmt = await self.user_filter(stmt, student_id)
-        count_records = await self._get_count_records(stmt)
-        records = await self._get_records(count_records, stmt, filters)
-        response = await self._convert_response(count_records, records, filters)
+        count_records = await self._get_count_records(stmt) # общее количество записей, соответствующих запросу stmt
+        records = await self._get_records(count_records, stmt, filters) # фактические записи, применяя ограничения из filters (например, пагинация).
+        response = await self._convert_response(count_records, records, filters) # Вызывает _convert_response, 
+        # чтобы привести данные в нужный формат для возврата.
         return response
